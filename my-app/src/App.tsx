155ca9fa8c88a5b75763748f58cbe941400a8426
brunohpdev <brunohp008@gmail.com>
2026-08-19 21:08:39 -0300

    feat: Criação de rotas

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,20 +1,22 @@
+import { Outlet } from "react-router";
 import Bruno from "./components/Bruno/Bruno";
+import Cabecalho from "./components/Cabecalho";
 
 export default function app() {
   //declarar constantes e variaveis
 
-  
+
   return (
     //aqui voce cria todo conteudo HTML de apresentação 
-
-
+    <Cabecalho /> 
+      </Outlet >
 
     <div>
 
       <h1>Meu Componente</h1>
-      <Bruno/>
+      <Bruno />
     </div>
   );
- 
+
 
 }
--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,9 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Home from './routes/Home/index.tsx'
+import Produto from './routes/produto/index.tsx'
+
+
+const router = createBrowserRouter([
+  {path: "/", element:  <App/>, errorElement: <Home/>, children=[
+    {path: "/", element: <Home/>},
+    { path: "/produto", element: <Produto /> },
+
+  ])
+}]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
