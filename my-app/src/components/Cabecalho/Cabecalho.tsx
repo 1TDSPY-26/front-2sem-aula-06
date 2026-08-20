f1d7a71d5dec0bea4402f53f5efeb63f127b0459
alvesdemacedoenzo-beep <alvesdemacedoenzo@gmail.com>
2026-08-19 21:09:59 -0300

    feat : adicionando coisas no app tsx

---
+++ my-app/src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,12 @@
+export default function Cabecalho(){
+
+    return(
+        <header>
+            <nav>
+                <li><a href="/">Home</a></li>
+                <li><a href="/produto">Produto</a></li>
+
+            </nav>
+        </header>
+    )
+}
---
+++ my-app/src/routes/Home/Produto/index.tsx
@@ -0,0 +1,7 @@
+export default function Produto() {
+    return (
+        <main>
+            <h2>Produto</h2>
+        </main>
+    );
+}
