af52128607968c892fb55ab38f84c0dda884f782
ElizaDuarte <elizadnascimento@gmail.com>
2026-08-19 21:08:49 -0300

    feat: Criação de rotas.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,14 @@
+import { Outlet } from "react-router";
 import Eliza from "./components/Eliza/Eliza";
+import Cabecalho from "./components/Cabecalho/Cabecalho";
 
 export default function name() {
   // Declarar constantes e variáveis
 
   return(
     <div>
-    {/** Aqui você cria todo conteúdo HTML de apresentação */}
-      <h1>Meu Componente</h1>
-      <Eliza sobreNome1="Duarte" sobreNome2="do Nascimento"/>
+    <Cabecalho />
+    <Outlet />
     </div>
   );
   
